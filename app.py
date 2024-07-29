3a53f642e77f245e483751af3e6172b91d7a297e
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-07-29 15:18:21 +0530

    Update-2 app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
 import streamlit as st
-
+import matplotlib.pyplot as plt
 st.title("Image Classification Of Fashion Items")
+# import streamlit as st
+
+uploaded_files = st.file_uploader("Upload Image")
+print(uploaded_files)
+# plt.figure()
+# plt.subplot(1,2,1)
+# plt.imshow(uploaded_files)
+# plot_image(i, predictions[i], test_labels, test_images)
+# plt.subplot(1,2,2)
+# plot_value_array(i, predictions[i],  test_labels)
+# plt.show()
