19deb960601ac4746b24e4b8a6952869a49571ba
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-07-29 16:02:47 +0530

    Update-8 app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import cv2
+import opencv-python-headless as oph
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,12 +8,12 @@ st.title("Image Classification Of Fashion Items")
 
 
 uploaded_file = st.file_uploader("Upload Image")
-image = cv2.imread(uploaded_file)   
+image = oph.imread(uploaded_file)   
 image=image[:, ::-1]
 # BGR -> RGB  
 #type(image.shape)
 # img2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)   
-small_img=cv2.resize(image, (28,28))
+small_img=oph.resize(image, (28,28))
 img=cv2.cvtColor(small_img, cv2.COLOR_BGR2GRAY,)
 # img=np.abs(256-img)
 fig1=plt.figure()
