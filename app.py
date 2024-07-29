3306d47a316b432282dd7d7f71d9f5435f1fe872
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-07-29 15:42:06 +0530

    Update-3 app.py

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 import streamlit as st
-# import matplotlib.pyplot as plt
-st.title("Image Classification Of Fashion Items")
-# import streamlit as st
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-uploaded_files = st.file_uploader("Upload Image")
-print(uploaded_files)
-# plt.figure()
-# plt.subplot(1,2,1)
-# plt.imshow(uploaded_files)
+
+st.title("Image Classification Of Fashion Items")
+
+
+uploaded_file = st.file_uploader("Upload Image")
+image = cv2.imread(uploaded_file)   
+image=image[:, ::-1]
+# BGR -> RGB  
+#type(image.shape)
+# img2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)   
+small_img=cv2.resize(image, (28,28))
+img=cv2.cvtColor(small_img, cv2.COLOR_BGR2GRAY,)
+# img=np.abs(256-img)
+plt.figure()
+plt.subplot(1,2,1)
+plt.imshow(img)
 # plot_image(i, predictions[i], test_labels, test_images)
 # plt.subplot(1,2,2)
 # plot_value_array(i, predictions[i],  test_labels)
-# plt.show()
+plt.show()
