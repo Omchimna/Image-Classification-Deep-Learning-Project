4f576c709caa24e5bba03cd6c2044d9982733f70
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-08-01 01:08:31 +0530

    Update-68 app.py

--- app.py
+++ app.py
@@ -35,15 +35,15 @@ def main():
 		probabilities = result[0]
 		# img=np.abs(256-img)
 
-	class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-	fig,ax=plt.subplots(1,2)
-	# ax.subplot(1,2,1)
-	ax[0].imshow(final)
-	# ax.subplot(1,2,2)
-	ax[1].bar(class_names, probabilities)
-	ax[1].xticks(range(10), class_names, rotation=45)
-	ax[1].ylabel('Probability')
-	st.pyplot(fig)
+		class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+		fig,ax=plt.subplots(1,2)
+		# ax.subplot(1,2,1)
+		ax[0].imshow(final)
+		# ax.subplot(1,2,2)
+		ax[1].bar(class_names, probabilities)
+		ax[1].xticks(range(10), class_names, rotation=45)
+		ax[1].ylabel('Probability')
+		st.pyplot(fig)
 
 if __name__ == '__main__':
 	main()
