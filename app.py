9408fae2665dab58a68c5c5b2e5f00c2eace3609
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-08-01 00:43:53 +0530

    Update-62 app.py

--- app.py
+++ app.py
@@ -12,11 +12,10 @@ def load_model():
 	return model
 
 def image_pre_pro(file):
-  # image = cv2.imread(file)
-  final_img = cv2.cvtColor(file.astype(np.uint8), cv2.COLOR_BGR2GRAY,)
-  small_img = cv2.resize(final_img, (28,28), )
-  return small_img
-
+	# image = cv2.imread(file)
+	final_img = cv2.cvtColor(file.astype(np.uint8), cv2.COLOR_BGR2GRAY,)
+	small_img = cv2.resize(final_img, (28,28), )
+	return small_img
 
 def predict_class(image, model):
 	pre_img = tf.convert_to_tensor(image)
@@ -25,29 +24,28 @@ def predict_class(image, model):
 	return prediction
 
 def main():
-  st.title("Image Classification Of Fashion Items")
-  st.text("This Works")  
-  uploaded_file = st.file_uploader("Upload Image" ,type=['jpg', 'png', 'jpeg'])
-  if uploaded_file is not None:
-  	img = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1) #if uploaded_file not None else np.array([[0]*28]*28)
-  	final = image_pre_pro(img)
-	model = load_model()
-	result = predict_class(final,model)
-	probabilities = result[0]
-	# img=np.abs(256-img)
-    
-  class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+	st.title("Image Classification Of Fashion Items")
+	st.text("This Works")  
+	uploaded_file = st.file_uploader("Upload Image" ,type=['jpg', 'png', 'jpeg'])
+	if uploaded_file is not None:
+		img = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1) #if uploaded_file not None else np.array([[0]*28]*28)
+		final = image_pre_pro(img)
+		model = load_model()
+		result = predict_class(final,model)
+		probabilities = result[0]
+		# img=np.abs(256-img)
+	    
+	class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                  'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
   
-  
-  fig,ax=plt.subplots()
-  # ax.subplot(1,2,1)
-  ax[0].imshow(final)
-  # ax.subplot(1,2,2)
-  ax[1].bar(class_names, probabilities)
-  ax[1].xticks(range(10), class_names, rotation=45)
-  ax[1].ylabel('Probability')
-  st.pyplot(fig)
+  	fig,ax=plt.subplots()
+	# ax.subplot(1,2,1)
+	ax[0].imshow(final)
+	# ax.subplot(1,2,2)
+	ax[1].bar(class_names, probabilities)
+	ax[1].xticks(range(10), class_names, rotation=45)
+	ax[1].ylabel('Probability')
+	st.pyplot(fig)
 
 if __name__ == '__main__':
-  main()
+	main()
