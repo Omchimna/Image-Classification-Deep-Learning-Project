249db301d3913cc7350a5c7232cb3f5e3be2b97e
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-08-01 00:48:18 +0530

    Update-65 app.py

--- app.py
+++ app.py
@@ -34,10 +34,9 @@ def main():
 		result = predict_class(final,model)
 		probabilities = result[0]
 		# img=np.abs(256-img)
-	    
+
 	class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-  
-  	fig,ax=plt.subplots()
+	fig,ax=plt.subplots()
 	# ax.subplot(1,2,1)
 	ax[0].imshow(final)
 	# ax.subplot(1,2,2)
