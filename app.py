5c734975b166c58e466dcf94859c9e24b39fb2c9
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-07-31 23:08:26 +0530

    Update-26 app.py

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ def image_pre_pro(file):
   image = cv2.imread(file)
   # img = Image.open(file)
   # img=np.array(image)
-  small_img = cv2.resize(img, (28,28))
+  small_img = cv2.resize(image, (28,28))
   # small_img=img.resize((28,28))
-  final_img= cv2.cvtColor(small_img, cv2.COLOR_BGR2GRA)
+  final_img = cv2.cvtColor(small_img, cv2.COLOR_BGR2GRA)
   return final_img
 
 def main():
