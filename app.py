33e6f583b4ba0bb8709358127a14dac81527863a
Om Chimna <110689335+Omchimna@users.noreply.github.com>
2024-07-31 23:31:59 +0530

    Update-32 app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from PIL import Image, ImageOps
 
 def image_pre_pro(file):
   image = cv2.imread(file)
-  final_img=cv2.cvtColor(image, cv2.COLOR_BGR2GRAY,)
+  final_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY,)
   small_img = cv2.resize(final_img, (28,28), )
   return small_img
 
